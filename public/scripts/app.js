96d239c7ff527a81057b10f9b60846ed45a2dc40
Andrew Nichols <tandrewnichols@gmail.com>
2013-09-16 23:38:46 -0400

    Update swig to new version

---
+++ public/scripts/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('arena', ['PingPongFilters']).
+	config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	$routeProvider.
+	when('/matches', {
+			templateUrl: 'partials/match-list.html',
+			controller: MatchListCtrl
+		}).
+		when('/matches/:matchId', {
+			templateUrl: 'partials/match-detail.html',
+			controller: MatchDetailCtrl
+		}).
+		when('/players', {
+			templateUrl: 'partials/users-list.html',
+			controller: PlayerListCtrl
+		}).
+		when('/players/:userId', {
+			templateUrl: 'partials/users-detail.html',
+			controller: PlayerDetailCtrl
+		}).
+		when('/updates', {
+			templateUrl: 'partials/recent-updates.html'
+		}).
+		otherwise({redirectTo: '/matches'});
+	//$locationProvider.html5Mode(true); // Use HTML5 strategy if available for how application deep linking paths are stored
+}]);
+
